fd4137461f58bab9bbcb0807e1dd4a30c039e51e
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-06-02 09:19:03 +0100

    Update

--- src/components/Cause.tsx
+++ src/components/Cause.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react"
+// import { useNavigate } from "react-router"
+// import { useState } from "react"
 import { CauseItems } from "../assets/assets"
 import Button from "./ui/Button"
 import { motion } from "framer-motion"
-import { useNavigate } from "react-router"
 
 
 
 const Cause = () => {
-  const [viewMore, setViewMore] = useState<boolean>(false)
+  // const [viewMore, setViewMore] = useState<boolean>(false)
 
-  const navigate = useNavigate() 
+  // const navigate = useNavigate() 
   return (
     <section className="bg-neutral-50 w-full p-4 ">
       <div
@@ -36,7 +36,7 @@ const Cause = () => {
                   <div className="flex flex-col gap-2 py-4 px-4">
                     <h2 className="font-bold text-orange-600 text-xl mb-2">{item.title}</h2>
                     <p className="text-sm font-semibold text-neutral-700 h-15 mb-2">{item.tag}</p>
-                    <Button size="md" variant="secondary" className="inline-block" onClick={() => navigate("#donate")}>Donate</Button>
+                    <Button size="md" variant="secondary" className="inline-block">Donate</Button>
                   </div>
                 </div>
               ))
